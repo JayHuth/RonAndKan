265bb5194de8db1bf9b79f9ec3667db35ed7d54d
Justin Huth <Jayhuth96@gmail.com>
2023-03-06 14:30:16 -0600

    Add project files.

---
+++ RonAndKan/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.Security;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
+using RonAndKan;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            QuoteGenorator.KanyeQuote();
+
+            QuoteGenorator.RonQuote();
+        }
+    }
+
+}
